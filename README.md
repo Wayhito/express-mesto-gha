a5715b8182d03c55ac6588e334e94acb8d42a8a7
Wayhito <moskariko@mail.ru>
2023-06-13 06:25:59 +0200

    Добавил все файлы

--- README.md
+++ README.md
@@ -14,3 +14,7 @@
 
 `npm run start` — запускает сервер   
 `npm run dev` — запускает сервер с hot-reload
+
+## Ссылка на проект репозиторий 
+
+https://github.com/Wayhito/express-mesto-gha.git
---
+++ routes/cards.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+  receiveCards,
+  createCard,
+  likeCard,
+  dislikeCard,
+  deleteCard,
+} = require('../controllers/cards');
+
+router.post('/', createCard);
+router.get('/', receiveCards);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
+router.delete('/:id', deleteCard);
+
+module.exports = router;
