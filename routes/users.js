c1b202e813f849f7eaffb169960ae25a2b7afa04
Wayhito <moskariko@mail.ru>
2023-06-27 11:33:32 +0200

    Joi-celebrate валидация в routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsersInfo,
@@ -12,14 +13,47 @@ const {
   registerUser,
 } = require('../controllers/users');
 
-router.get('/me', getCurrentUserInfo);
 router.get('/', getUsersInfo);
-router.get('/:id', getUserInfo);
+router.get('/me', getCurrentUserInfo);
 
-router.patch('/me', setUserInfo);
-router.patch('/me/avatar', setUserAvatar);
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().min(24).max(24),
+  }),
+}), getUserInfo);
 
-router.post('/signin', loginUser);
-router.post('/signup', registerUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), setUserInfo);
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi
+      .string()
+      .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), setUserAvatar);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+  }),
+}), loginUser);
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi
+      .string()
+      .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), registerUser);
 
 module.exports = router;
