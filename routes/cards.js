fb2e8b0cb837a1e25b7654f6655ea4af603ae3a6
Wayhito <moskariko@mail.ru>
2023-06-27 16:35:53 +0200

    Переделал joi валидацию на id карточек и user'а

--- routes/cards.js
+++ routes/cards.js
@@ -24,19 +24,19 @@ router.get('/', receiveCards);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().min(24).max(24),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().min(24).max(24),
+    cardId: Joi.string().length(24).hex().required(),
   }),
 }), dislikeCard);
 
 router.delete('/:id', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().min(24).max(24),
+    id: Joi.string().length(24).hex().required(),
   }),
 }), deleteCard);
 
