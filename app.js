b25b1a3d6562c40e5659172a4a278cba888c2338
Wayhito <moskariko@mail.ru>
2023-06-28 11:53:01 +0200

    Fixes: app.js

--- app.js
+++ app.js
@@ -1,65 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
-const limiter = require('./middlewares/rateLimiter');
-
-const routeSignup = require('./routes/users');
-const routeSignin = require('./routes/users');
-
-const auth = require('./middlewares/auth');
-
-const routeUsers = require('./routes/users');
-const routeCards = require('./routes/cards');
-
-const NotFoundError = require('./errors/NotFound');
+const { routes } = require('./routes');
+const { handleError } = require('./middlewares/handleError');
 
 const URL = 'mongodb://127.0.0.1:27017/mestodb';
 const { PORT = 3000 } = process.env;
 
-mongoose.set('strictQuery', true);
-mongoose.connect(URL);
-
 const app = express();
 
-app.use(helmet());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 минут
+  max: 100, // 100 запросов с одного IP
+});
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log(`Connected to database on ${URL}`);
+  })
+  .catch((err) => {
+    console.log('Error on database connection');
+    console.error(err);
+  });
 
 app.use(limiter);
 
-app.use('/', routeSignup);
-app.use('/', routeSignin);
+app.use(helmet());
 
-app.use(auth);
-
-app.use('/users', routeUsers);
-app.use('/cards', routeCards);
-
-app.use((req, res, next) => next(new NotFoundError('Страницы по запрошенному URL не существует')));
+app.use(routes);
 
 app.use(errors());
 
-app.use((err, _, res, next) => {
-  if (err.name === 'CastError' || err.name === 'ValidationError') {
-    const { statusCode = 400 } = err;
+app.use(handleError);
 
-    return res.status(statusCode).send({ message: 'Переданы некорректные данные' });
-  }
-
-  if (err.name === 'Error') return res.status(err.statusCode).send({ message: err.message });
-
-  if (err.code === 11000) {
-    const { statusCode = 409 } = err;
-
-    return res.status(statusCode).send({ message: 'Пользователь с таким электронным адресом уже зарегистрирован' });
-  }
-
-  const { statusCode = 500 } = err;
-  return next(res.status(statusCode).send({ message: 'На сервере произошла ошибка' }));
+app.listen(PORT, () => {
+  console.log(`App started on port ${PORT}`);
 });
-
-app.listen(PORT);
